4d3f0515f8b7ad2de9d10ec7fe8e3fc1d53e1215
ADMIN <ADMIN@DESKTOP-BN1GJNU.bbrouter>
2021-08-12 22:49:39 +0530

    My commit

---
+++ balu/src/main/java/com/balu/demo/BaluApplication.java
@@ -0,0 +1,13 @@
+package com.balu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaluApplication.class, args);
+	}
+
+}
